54e506b9e576b8ffcf39d88f608d5dd91499ff5b
Tom Molesworth <TEAM@cpan.org>
2017-02-23 17:49:27 +0800

    Include common base class for data types

---
+++ lib/Net/Async/Trello/Common.pm
@@ -0,0 +1,20 @@
+package Net::Async::Trello::Common;
+use strict;
+use warnings;
+
+=head2 new
+
+Instantiates.
+
+=cut
+
+sub new {
+	my $self = bless { @_[1..$#_] }, $_[0];
+	die "no ->trello provided" unless $self->trello;
+	$self
+}
+
+sub trello { shift->{trello} }
+
+1;
+
